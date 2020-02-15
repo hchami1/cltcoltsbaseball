41e0bea124a24347b2e8beca44c7e96a6705ac22
Hadi Chami <hchami11@gmail.com>
2020-02-15 12:37:20 -0500

    adding build folder

---
+++ build/precache-manifest.6a73105ce113f82c3786d2ba9cc6d28c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61900a59f820b9903d0d6d49c8ffa1d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8715ad4559d525f657d",
+    "url": "/static/css/2.dad59c94.chunk.css"
+  },
+  {
+    "revision": "81ac67eea1a839def749",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "c8715ad4559d525f657d",
+    "url": "/static/js/2.1a6bac73.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.1a6bac73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ac67eea1a839def749",
+    "url": "/static/js/main.068297f1.chunk.js"
+  },
+  {
+    "revision": "28cebcfe52934e654768",
+    "url": "/static/js/runtime-main.80e2138c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "045adc536d00d9778e04342d9a18b17d",
+    "url": "/static/media/logo.045adc53.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
